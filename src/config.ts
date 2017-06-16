7622d57c2db0303a3140911e911aa1d0cbcd4a6c
Maximo Dominguez <mamodom@gmail.com>
2017-06-16 02:47:22 -0400

    Adding a .bablerc for react-native-cli to use inline-environment-variables and inline env variables provided by dotenv

---
+++ src/config.ts
@@ -0,0 +1,7 @@
+export const API_URL = process.env.API_URL || 'http://api.wwmoderator.com/';
+export const PUSHER_KEY = process.env.PUSHER_KEY || '';
+
+export const config = {
+    API_URL,
+    PUSHER_KEY,
+}
