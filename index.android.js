157a20f18fc31a2033e1bb65fdf60ee756cdc44b
Tomas Veras <tomasverasm@gmail.com>
2018-05-12 23:05:01 -0400

    New project using CRNA.

--- index.android.js
+++
@@ -1,4 +0,0 @@
-import { AppRegistry } from 'react-native';
-import { App } from './build';
-
-AppRegistry.registerComponent('werewolfVirtualCards', () => App);
--- src/actions/index.ts
+++
@@ -1,4 +0,0 @@
-import * as PartyActions from './party';
-import * as UserActions from './user';
-
-export const ActionCreators = Object.assign({}, PartyActions, UserActions);
