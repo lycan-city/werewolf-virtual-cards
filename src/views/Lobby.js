45e0304f01e6c9bd40dce057ee46a6edacd757ab
Tomas Veras <tomasverasm@gmail.com>
2018-05-22 01:19:17 -0400

    Adding Lobby View.

---
+++ src/views/Lobby.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { Container, Header, Body, Title, Content, Item, Input, Label, Button, Text, Icon, View, Separator, ListItem } from 'native-base';
+import QRCode from 'react-native-qrcode';
+
+export default class Join extends Component {
+
+  render() {
+    return (
+      <Container>
+        <Header> 
+          <Body>
+              <Title>Moe's Party</Title>
+          </Body>
+        </Header>
+        <Content>
+          <View style={styles.qrContainer}>
+            <Label style={styles.qrLabel}> A93F32A </Label>
+            <QRCode
+              value={'wwvc://A93F32A'}
+              size={200}
+              bgColor='black'
+              fgColor='white'/>
+          </View>
+           <Separator bordered>
+            <Text>Players</Text>
+          </Separator>
+          <ListItem >
+            <Text>Aaron Bennet</Text>
+            <Button iconLeft transparent danger>
+              <Icon type="FontAwesome" name="ban" />
+            </Button>
+            <Button iconLeft transparent warning>
+              <Icon type="Foundation" name="crown" />
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Text>Claire Barclay</Text>
+            <Button iconLeft transparent danger>
+              <Icon type="FontAwesome" name="ban" />
+            </Button>
+          </ListItem>
+          <ListItem last>
+            <Text>Kelso Brittany</Text>
+            <Button iconLeft transparent danger>
+              <Icon type="FontAwesome" name="ban" />
+            </Button>
+          </ListItem>
+          <ListItem>
+            <Text>Caroline Arts</Text>
+            <Button iconLeft transparent danger>
+              <Icon type="FontAwesome" name="ban" />
+            </Button>
+          </ListItem>
+          <Button block bordered warning style={styles.button}>
+            <Text>Prepare</Text>
+          </Button>
+          <Button block bordered success style={styles.button}>
+            <Text>Start</Text>
+          </Button>
+          <Button block bordered danger style={styles.button}>
+            <Text>Flee</Text>
+          </Button>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  qrContainer: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 15,
+    marginBottom: 15,
+  },
+  qrLabel: {
+    fontSize: 20,
+    marginBottom: 5,
+  },
+  row: {
+    flexDirection: 'row'
+  },
+  button: {
+    marginTop: 10,
+    marginLeft: 10,
+    marginRight: 10,
+  },
+});
