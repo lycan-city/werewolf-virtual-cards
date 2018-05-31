a342692b0e13f13c5c5a992ff892adaf66805e2e
Tomas Veras <tomasverasm@gmail.com>
2018-05-31 17:00:51 -0400

    Adding Game View Host.

---
+++ src/views/Game.js
@@ -0,0 +1,118 @@
+import React, { Component } from 'react';
+import { StyleSheet } from 'react-native';
+import { Container, Header, Body, Title, Content, Thumbnail, Button, List, ListItem, Left, Icon, Right, Text } from 'native-base';
+
+export default class Game extends Component {
+  render() {
+    return (
+      <Container>
+        <Header> 
+          <Body>
+						<Title>Virtual Cards</Title>
+          </Body>
+        </Header>
+        <Content>
+					<List>
+            <ListItem itemHeader first>
+              <Text>Players</Text>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require('../thumbnails/full-moon.jpg')} />
+              </Left>
+              <Body>
+                <Text style={styles.name}>Steve</Text>
+                <Text style={styles.role} note>Villager</Text>
+              </Body>
+              <Right>
+                <Button bordered danger>
+                  <Icon type="Foundation" name="skull" style={styles.icon} />
+                </Button>
+              </Right>
+            </ListItem>
+						<ListItem avatar>
+              <Left>
+                <Thumbnail source={require('../thumbnails/full-moon.jpg')} />
+              </Left>
+              <Body>
+                <Text style={styles.name} >Jane</Text>
+                <Text style={styles.role} note>Seer</Text>
+              </Body> 
+              <Right>
+                <Button bordered danger>
+                  <Icon type="Foundation" name="skull" style={styles.icon}/>
+                </Button>
+              </Right>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require('../thumbnails/full-moon.jpg')} />
+              </Left>
+              <Body>
+                <Text style={styles.name} >Bill</Text>
+                <Text style={styles.role} note>Bodyguard</Text>
+              </Body> 
+              <Right>
+                <Button bordered danger>
+                  <Icon type="Foundation" name="skull" style={styles.icon}/>
+                </Button>
+              </Right>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require('../thumbnails/full-moon.jpg')} />
+              </Left>
+              <Body>
+                <Text style={styles.name} >Mike</Text>
+                <Text style={styles.role} note>Werewolf</Text>
+              </Body> 
+              <Right>
+                <Button bordered danger>
+                  <Icon type="Foundation" name="skull" style={styles.icon}/>
+                </Button>
+              </Right>
+            </ListItem>
+            <ListItem avatar>
+              <Left>
+                <Thumbnail source={require('../thumbnails/full-moon.jpg')} />
+              </Left>
+              <Body>
+                <Text style={styles.name} >Paco</Text>
+                <Text style={styles.role} note>Villager</Text>
+              </Body> 
+              <Right>
+                <Button bordered danger>
+                  <Icon type="Foundation" name="skull" style={styles.icon}/>
+                </Button>
+              </Right>
+            </ListItem>
+            <ListItem itemHeader>
+              <Text>Script</Text>
+            </ListItem>
+            <ListItem>
+              <Text>Wake up Bodyguard, choose a player to protect.</Text>
+            </ListItem>
+            <ListItem>
+              <Text>Werewolves, choose a player to kill.</Text>
+            </ListItem>
+            <ListItem>
+              <Text>Wake up Seer, check a player.</Text>
+            </ListItem>
+          </List>
+        </Content>
+      </Container>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  icon: {
+    fontSize: 34,
+  },
+  name: {
+    fontSize: 22,
+  },
+  role:{
+    fontSize: 17,
+  }
+});
