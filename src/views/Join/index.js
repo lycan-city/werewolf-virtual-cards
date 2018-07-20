40f7e2652d1e942886a99b20b29f110e1c8c5b17
Willy Ovalle <willyovalle16@gmail.com>
2018-07-20 02:22:10 -0400

    feat: adding join party

--- src/views/Join/index.js
+++ src/views/Join/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Linking } from 'expo';
 import {
   Container,
   Header,
@@ -14,6 +15,8 @@ import {
   View,
 } from 'native-base';
 
+import { Camera, Permissions } from 'expo';
+import { TouchableOpacity, Alert } from 'react-native';
 import styles from './styles';
 import Db from '../../db';
 
@@ -22,6 +25,9 @@ export default class Join extends Component {
     super();
     this.state = {
       partyId: '',
+      hasCameraPermission: null,
+      openCamera: false,
+      invalidCameraState: false,
     };
 
     this.db = Db.get();
@@ -30,6 +36,17 @@ export default class Join extends Component {
   static navigationOptions = {
     title: 'Virtual Cards',
   };
+
+  componentDidMount() {
+    this.setState(
+      {
+        partyId: this.props.navigation.state.params.partyId || '',
+      },
+      () => {
+        if (this.state.partyId) this.getParty();
+      }
+    );
+  }
 
   onChangeText = partyId => {
     this.setState({
@@ -41,8 +58,6 @@ export default class Join extends Component {
     const { partyId } = this.state;
     const party = await this.db.getPartyById(partyId);
 
-    console.log({ party });
-
     if (!party) {
       alert(`No party with id ${partyId}`);
       return;
@@ -51,7 +66,72 @@ export default class Join extends Component {
     this.props.navigation.navigate('Lobby', { party });
   };
 
+  openCamera = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+
+    if (status !== 'granted') {
+      alert(
+        'Camera permissions have been denied. Go into your settings and allow camera access.'
+      );
+    }
+
+    this.setState({
+      hasCameraPermission: status === 'granted',
+      openCamera: status === 'granted',
+    });
+  };
+
+  onBarCodeRead = qr => {
+    if (this.state.invalidCameraState) {
+      return;
+    }
+
+    const partyId = Linking.parse(qr.data).queryParams.id;
+
+    if (!partyId) {
+      this.setState({ invalidCameraState: true });
+      Alert.alert('Invalid QR', `QR: ${qr.data} is not a valid party`, [
+        {
+          text: 'OK',
+          onPress: () => this.setState({ invalidCameraState: false }),
+        },
+      ]);
+      return;
+    }
+
+    this.setState(
+      {
+        partyId,
+        openCamera: false,
+      },
+      () => this.getParty()
+    );
+  };
+
   render() {
+    if (this.state.openCamera) {
+      return (
+        <Camera
+          type={Camera.Constants.Type.back}
+          style={{ flex: 1 }}
+          onBarCodeRead={this.onBarCodeRead}
+        >
+          <View>
+            <TouchableOpacity
+              onPress={() => {
+                this.setState({
+                  type:
+                    this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                });
+              }}
+            />
+          </View>
+        </Camera>
+      );
+    }
+
     return (
       <Container>
         <Content scrollEnabled={false} contentContainerStyle={styles.content}>
@@ -69,7 +149,7 @@ export default class Join extends Component {
             bordered
             info
             style={styles.buttonBlock}
-            onPress={() => this.props.navigation.navigate('Lobby')}
+            onPress={this.openCamera}
           >
             <Text>Scan</Text>
             <Icon name="md-qr-scanner" />
