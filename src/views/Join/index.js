fe99b754ddca7f01fcec42e0e57fcb7e46434fd0
Willy Ovalle <willyovalle16@gmail.com>
2018-07-20 00:24:48 -0400

    feat: join parties via code

--- src/views/Join/index.js
+++ src/views/Join/index.js
@@ -1,19 +1,67 @@
 import React, { Component } from 'react';
-import { Container, Header, Body, Title, Content, Item, Input, Label, Button, Text, Icon, View } from 'native-base';
+import {
+  Container,
+  Header,
+  Body,
+  Title,
+  Content,
+  Item,
+  Input,
+  Label,
+  Button,
+  Text,
+  Icon,
+  View,
+} from 'native-base';
+
 import styles from './styles';
+import Db from '../../db';
 
 export default class Join extends Component {
+  constructor() {
+    super();
+    this.state = {
+      partyId: '',
+    };
+
+    this.db = Db.get();
+  }
+
   static navigationOptions = {
     title: 'Virtual Cards',
+  };
+
+  onChangeText = partyId => {
+    this.setState({
+      partyId,
+    });
+  };
+
+  getParty = async () => {
+    const { partyId } = this.state;
+    const party = await this.db.getPartyById(partyId);
+
+    console.log({ party });
+
+    if (!party) {
+      alert(`No party with id ${partyId}`);
+      return;
+    }
+
+    this.props.navigation.navigate('Lobby', { party });
   };
 
   render() {
     return (
       <Container>
         <Content scrollEnabled={false} contentContainerStyle={styles.content}>
-          <Item floatingLabel >
-            <Label> Party Code </Label>
-            <Input />
+          <Item floatingLabel>
+            <Label>Party Code</Label>
+            <Input
+              value={this.state.partyId}
+              onChangeText={this.onChangeText}
+              required
+            />
           </Item>
           <Button
             iconRight
@@ -23,8 +71,8 @@ export default class Join extends Component {
             style={styles.buttonBlock}
             onPress={() => this.props.navigation.navigate('Lobby')}
           >
-            <Text> Scan </Text>
-            <Icon name='md-qr-scanner' />
+            <Text>Scan</Text>
+            <Icon name="md-qr-scanner" />
           </Button>
           <View style={styles.row}>
             <Button
@@ -34,18 +82,18 @@ export default class Join extends Component {
               style={styles.button}
               onPress={() => this.props.navigation.goBack()}
             >
-              <Icon name='arrow-back'/>
-              <Text> Back </Text>
+              <Icon name="arrow-back" />
+              <Text>Back</Text>
             </Button>
             <Button
               iconRight
               bordered
               success
               style={styles.button}
-              onPress={() => this.props.navigation.navigate('Lobby')}
+              onPress={this.getParty}
             >
-              <Text> Join </Text>
-              <Icon name='add'/>
+              <Text>Join</Text>
+              <Icon name="add" />
             </Button>
           </View>
         </Content>
