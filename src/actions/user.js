033958e1e8a0957344b233642a606ed8199d1d9d
Tomas Veras <tomasverasm@gmail.com>
2018-09-04 06:02:48 -0400

    refactor: setting moderator.

--- src/actions/user.js
+++ src/actions/user.js
@@ -6,3 +6,8 @@ export const setUsername = username => ({
   type: types.user.set.name,
   username,
 });
+
+export const setModerator = username => ({
+  type: types.user.set.as.moderator,
+  username,
+});
--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,9 +1,15 @@
 import types from '../actions';
 
-const user = (state = {}, action) => {
+const defaultUser = {
+  attributes: { moderator: false },
+};
+
+const user = (state = defaultUser, action) => {
   switch (action.type) {
     case types.user.set.name:
       return { ...state, username: action.username };
+    case types.user.set.as.moderator:
+      return { username: action.username, attributes: { ...state.attributes, moderator: true } };
     default:
       return state;
   }
