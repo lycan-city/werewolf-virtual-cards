5136f012617528624c72750065e2084fe80a4463
Tomas Veras <tomasverasm@gmail.com>
2018-09-06 21:29:12 -0400

    wip: integrating werewolf-brain.

--- src/views/Prepare/index.js
+++ src/views/Prepare/index.js
@@ -17,6 +17,7 @@ import {
   Footer,
   FooterTab,
 } from 'native-base';
+import brain from 'werewolf-brain';
 import propTypes from 'prop-types';
 import styles from './styles';
 
@@ -27,22 +28,20 @@ class Prepare extends Component {
 
   constructor(props) {
     super(props);
+    this.decks = brain.getDecks();
     this.state = {
-      template: 'novice',
+      template: 'basic',
       lang: 'en',
       mode: 'normal',
-      deck: {
-        apprentice_seer: 1,
-        aura_seer: 0,
-        bodyguard: 1,
-        cult_leader: 0,
-        lycan: 1,
-      },
+      deck: this.decks.basic,
+      detailedDeck: brain.translateDeck(this.decks.basic),
     };
   }
 
   onTemplateChange(template) {
-    this.setState({ template });
+    const deck = this.decks[template];
+    const detailedDeck = brain.translateDeck(deck);
+    this.setState({ template, deck, detailedDeck });
   }
 
   onLangChange(lang) {
@@ -75,8 +74,28 @@ class Prepare extends Component {
   render() {
     const { navigation } = this.props;
     const {
-      template, lang, mode, deck
+      template, lang, mode, deck, detailedDeck
     } = this.state;
+    const decks = Object.keys(this.decks).map(d => <Picker.Item key={d} label={d} value={d} />);
+    const currentDeck = detailedDeck.map(c => (
+      <ListItem icon key={c.key}>
+        <Left>
+          <Switch value={deck[c.key] > 0} onValueChange={() => this.cardToggle(c.key)} />
+        </Left>
+        <Body>
+          <Text>{c.role}</Text>
+        </Body>
+        <Right>
+          <Button transparent danger onPress={() => this.decreaseCard(c.key)}>
+            <Icon type="Feather" name="minus-square" />
+          </Button>
+          <Text> {deck[c.key]} </Text>
+          <Button transparent success onPress={() => this.increaseCard(c.key)}>
+            <Icon type="Feather" name="plus-square" />
+          </Button>
+        </Right>
+      </ListItem>
+    ));
     return (
       <Container style={styles.content}>
         <Content>
@@ -92,14 +111,7 @@ class Prepare extends Component {
                 selectedValue={template}
                 onValueChange={t => this.onTemplateChange(t)}
               >
-                <Picker.Item label="All" value="all" />
-                <Picker.Item label="Amateur" value="amateur" />
-                <Picker.Item label="Basic" value="basic" />
-                <Picker.Item label="Competent" value="competent" />
-                <Picker.Item label="Novice" value="novice" />
-                <Picker.Item label="Vampires" value="vampires" />
-                <Picker.Item label="Wolfpack" value="wolfpack" />
-                <Picker.Item label="Custom" value="custom" />
+                {decks}
               </Picker>
             </View>
             <View style={styles.row}>
@@ -146,115 +158,7 @@ class Prepare extends Component {
               <ListItem itemHeader>
                 <Text style={styles.templateText}>Current Deck</Text>
               </ListItem>
-              <ListItem itemDivider>
-                <Text>A</Text>
-              </ListItem>
-              <ListItem icon>
-                <Left>
-                  <Switch
-                    value={deck.apprentice_seer > 0}
-                    onValueChange={() => this.cardToggle('apprentice_seer')}
-                  />
-                </Left>
-                <Body>
-                  <Text>Apprentice Seer</Text>
-                </Body>
-                <Right>
-                  <Button transparent danger onPress={() => this.decreaseCard('apprentice_seer')}>
-                    <Icon type="Feather" name="minus-square" />
-                  </Button>
-                  <Text> {deck.apprentice_seer} </Text>
-                  <Button transparent success onPress={() => this.increaseCard('apprentice_seer')}>
-                    <Icon type="Feather" name="plus-square" />
-                  </Button>
-                </Right>
-              </ListItem>
-              <ListItem icon>
-                <Left>
-                  <Switch
-                    value={deck.aura_seer > 0}
-                    onValueChange={() => this.cardToggle('aura_seer')}
-                  />
-                </Left>
-                <Body>
-                  <Text>Aura Seer</Text>
-                </Body>
-                <Right>
-                  <Button transparent danger onPress={() => this.decreaseCard('aura_seer')}>
-                    <Icon type="Feather" name="minus-square" />
-                  </Button>
-                  <Text> {deck.aura_seer} </Text>
-                  <Button transparent success onPress={() => this.increaseCard('aura_seer')}>
-                    <Icon type="Feather" name="plus-square" />
-                  </Button>
-                </Right>
-              </ListItem>
-              <ListItem itemDivider>
-                <Text>B</Text>
-              </ListItem>
-              <ListItem icon>
-                <Left>
-                  <Switch
-                    value={deck.bodyguard > 0}
-                    onValueChange={() => this.cardToggle('bodyguard')}
-                  />
-                </Left>
-                <Body>
-                  <Text>Bodyguard</Text>
-                </Body>
-                <Right>
-                  <Button transparent danger onPress={() => this.decreaseCard('bodyguard')}>
-                    <Icon type="Feather" name="minus-square" />
-                  </Button>
-                  <Text> {deck.bodyguard} </Text>
-                  <Button transparent success onPress={() => this.increaseCard('bodyguard')}>
-                    <Icon type="Feather" name="plus-square" />
-                  </Button>
-                </Right>
-              </ListItem>
-              <ListItem itemDivider>
-                <Text>C</Text>
-              </ListItem>
-              <ListItem icon>
-                <Left>
-                  <Switch
-                    value={deck.cult_leader > 0}
-                    onValueChange={() => this.cardToggle('cult_leader')}
-                  />
-                </Left>
-                <Body>
-                  <Text>Cult Leader</Text>
-                </Body>
-                <Right>
-                  <Button transparent danger onPress={() => this.decreaseCard('cult_leader')}>
-                    <Icon type="Feather" name="minus-square" />
-                  </Button>
-                  <Text> {deck.cult_leader} </Text>
-                  <Button transparent success onPress={() => this.increaseCard('cult_leader')}>
-                    <Icon type="Feather" name="plus-square" />
-                  </Button>
-                </Right>
-              </ListItem>
-              <ListItem itemDivider>
-                <Text>L</Text>
-              </ListItem>
-              <ListItem icon>
-                <Left>
-                  <Switch value={deck.lycan > 0} onValueChange={() => this.cardToggle('lycan')} />
-                </Left>
-                <Body>
-                  <Text>Lycan</Text>
-                </Body>
-                <Right>
-                  <Button transparent danger onPress={() => this.decreaseCard('lycan')}>
-                    <Icon type="Feather" name="minus-square" />
-                  </Button>
-                  <Text> {deck.lycan} </Text>
-                  <Button transparent success onPress={() => this.increaseCard('lycan')}>
-                    <Icon type="Feather" name="plus-square" />
-                  </Button>
-                </Right>
-              </ListItem>
+              {currentDeck}
             </List>
           </Form>
         </Content>
