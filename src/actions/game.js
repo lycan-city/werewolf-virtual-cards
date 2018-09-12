ccd598c9f66b000f15960532bbb3f8a54d933058
Tomas Veras <tomasverasm@gmail.com>
2018-09-11 23:58:31 -0400

    wip: assigning cards to players.

--- src/actions/game.js
+++ src/actions/game.js
@@ -8,6 +8,29 @@ const setGame = game => ({
   game,
 });
 
+const assignCardsToPlayers = (deck, players) => {
+  const cards = [].concat(
+    ...deck.map((c) => {
+      const deckCards = [];
+      for (let i = 0; i < c.amount; i += 1) {
+        deckCards.push({
+          description: c.description,
+          key: c.key,
+          role: c.role,
+        });
+      }
+      return deckCards;
+    })
+  );
+
+  for (let i = cards.length - 1; i > 0; i -= 1) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [cards[i], cards[j]] = [cards[j], cards[i]];
+  }
+
+  return Object.assign({}, ...Object.keys(players).map(k => ({ [k]: cards.pop() })));
+};
+
 export const createGame = () => (dispatch, getState) => {
   const {
     party: { players },
@@ -15,6 +38,7 @@ export const createGame = () => (dispatch, getState) => {
   } = getState();
 
   const game = brain.getGame(Object.keys(players).length, settings);
+  const playersWithCards = assignCardsToPlayers(game.deck, players);
   dispatch(setGame(game));
   NavigationService.navigate('Game');
 };
