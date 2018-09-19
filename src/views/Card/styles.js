31259aec4010d28ab9057576b533992b5b52c7e5
Tomas Veras <tomasverasm@gmail.com>
2018-09-19 02:17:22 -0400

    feat: adding cards role and description.

--- src/views/Card/styles.js
+++ src/views/Card/styles.js
@@ -8,4 +8,18 @@ export default StyleSheet.create({
   card: {
     backgroundColor: '#101f21',
   },
+  role: {
+    color: '#fff',
+    fontSize: 20,
+    position: 'absolute',
+    left: 5,
+    top: '89%',
+  },
+  description: {
+    color: '#fff',
+    position: 'absolute',
+    fontSize: 15,
+    left: 5,
+    top: '93%',
+  },
 });
