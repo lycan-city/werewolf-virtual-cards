d51ace3076eb176362673d9e6d5c4697b3544737
Maximo Dominguez <mamodom@gmail.com>
2018-09-21 18:12:21 -0400

    Adding storageBucket to config

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ export default {
     apiKey: process.env.FIREBASE_API_KEY,
     databaseURL: process.env.FIREBASE_DATABASE_URL,
     projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
   },
 };
